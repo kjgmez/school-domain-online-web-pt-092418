6c96b6455c4da7d24b8fd6390967ebd45cca8f1c
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-12 23:39:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,6 @@ class School
     @school_name= school_name
     @roster={}
   end
-  # def add_student(student_name, grade)
-  #   roster[grade] ||= []
-  #   roster[grade] << student_name
-  # end
   def add_student(name, grade)
     binding.pry
     @roster.include?(grade) ? @roster[grade] << name : @roster[grade]=[name] #ternary == conditional ? if true : if fasle (else)
@@ -20,3 +16,9 @@ class School
     @roster.each_value{|names| names.sort!} #sort! will change the value we are sorting. sort wont return sorted
   end
 end
+
+
+# def add_student(student_name, grade)
+#   roster[grade] ||= []
+#   roster[grade] << student_name
+# end
